2ffee41f783a24f6543916c9b2d6cab529f3c7e1
MubashirM-Ibrahim <officialmubashiribrahim77@gmail.com>
2022-11-19 10:59:42 +0500

    01-intro-to-js

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+
+// ///////////////////////////////////////////////////////////////
+// Replit Feature Generate Code ....
+// Addd three Numbers .
+
+function addThreeNum(a , b, c) {
+    
+    return a +  b +c ;
+}  
+
+let addThreeNumOutput = addThreeNum(1 , 2 , 54) ;
+
+console.log(addThreeNumOutput);
